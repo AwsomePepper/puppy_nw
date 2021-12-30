4e4a50ba802926e94e38279181352a89cd1530e5
HanHoing <hhy6789@gmail.com>
2021-12-30 09:32:32 +0900

    main화면 html react변환

---
+++ src/Main/footer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        );
+    }
+
+}
+
+export { Footer };
