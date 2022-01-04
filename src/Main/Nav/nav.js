a1306f626b9377446e5015c55eb7be0f13550724
AwsomePepper <glglwhgdk123@naver.com>
2022-01-04 12:24:02 +0900

    호잉짱짱

--- src/Main/Nav/nav.js
+++ src/Main/Nav/nav.js
@@ -12,7 +12,9 @@ function Nav()  {  //메뉴
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item"><a className="nav-link" href="#">Map</a></li>
+              <li className="nav-item"><a className="nav-link" href="/map">Map</a></li>
+              <li className="nav-item"><a className="nav-link" href="/signin">Log In</a></li>
+              <li className="nav-item"><a className="nav-link" href="/signup">Sign Up</a></li>
             </ul>
           </div>
         </div>
