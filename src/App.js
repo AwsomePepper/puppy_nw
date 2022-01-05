515cce96ce4254625320f28635413d6d65db3c19
haeseo <gotj999@naver.com>
2022-01-05 16:16:51 +0900

    슬라이드 배너 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { component, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import  Footer  from './Main/Footer/footer';
@@ -10,6 +12,9 @@ import { Content } from './Main/Content/content';
 import SignUp from './Main/Header/SignUp';
 import SignIn from './Main/Header/SignIn';
 import Image from './Main/Content/Image';
+import Content2 from './Main/Content/Content2';
+import React from 'react';
+
 
 
 function App() {
@@ -31,6 +36,8 @@ function App() {
           <Route path="/signin" element={<SignIn />}></Route>
         </Routes>
       </BrowserRouter>
+      <Content2 />
+          <Footer></Footer>
 
     </div>
   );
---
+++ src/Main/Content/Content2.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import icon2 from './신규회원 배너.png';
+import icon3 from './사료 배너 .png';
+import icon4 from './산책 배너.png';
+import { Carousel } from 'react-bootstrap';
+
+
+
+const Content2 = () => { //슬라이드 배너 
+
+    return (
+      <div style={{ display: 'block', width: 800, padding: -20,
+     margin: "auto" }}> 
+      <Carousel variant="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {icon2}
+            alt="First slide"
+            />
+          
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {icon3}
+            alt="Second slide"
+          />
+        
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src= {icon4}
+            alt="Third slide"
+          />
+         
+        </Carousel.Item>
+      </Carousel>
+    </div>
+    )
+  }
+
+  export default Content2;
+
+
+
+  //script오류 = npm install, bootstrap 오류 = npm install bootstrap
