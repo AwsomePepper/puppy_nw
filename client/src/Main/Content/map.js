70620e6657a1c3b4df7cc5fdcc5a81fdef0f0f22
HanHoing <hhy6789@gmail.com>
2022-01-07 17:36:23 +0900

    마커이미지 수정

--- client/src/Main/Content/map.js
+++ client/src/Main/Content/map.js
@@ -40,7 +40,7 @@ export default function Map({ chat }) {
     //마커찍기 + 인포윈도우 
     const markAdd = (user, lat, lng) => {
         // 마커를 생성합니다
-        var imageSrc = './marker.png', // 마커이미지의 주소입니다    
+        var imageSrc = './images/marker.png', // 마커이미지의 주소입니다    
             imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
             imageOption = { offset: new kakao.maps.Point(0, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니
 
